ff6edf311a1d9e696b9fc330c92eee5275f4f870
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-03 19:17:51 +0100

    Add files via upload

--- hello.c
+++ hello.c
@@ -1,25 +1,27 @@
-#include<stdlib.h>
-#include<stdio.h>
-
-int sub(int a, int b){
-     return a - b;
+class Main(){
+     int sub(int a, int b){
+         return a - b;
      
 
-}
+     }
 
 
-int add(int a, int b){
-     return a + b;
+     int add(int a, int b){
+          return a + b;
      
 
-}
+     }
 
-int bcc(int a){
-    int b=0;
-    if(a==0){b=1};
-}
+    int bcc(int a){
+        int b=0;
+        if(a==0){
+            b=1;
+        }
+    }
 
-int main(){
-    printf("hello world...\n");
-    return 0;
+    int main(){
+        printf("hello world...\n");
+        return 0;
+    }
+
 }
